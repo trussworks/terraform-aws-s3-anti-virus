9bce951993a0b76dc83c56c12ffb09cca202a4df
Chris Gilmer <chris@truss.works>
2019-10-28 16:26:51 -0700

    Allow s3:GetObjectVersion for scanning

--- anti-virus-scan.tf
+++ anti-virus-scan.tf
@@ -43,6 +43,7 @@ data "aws_iam_policy_document" "main_scan" {
     actions = [
       "s3:GetObject",
       "s3:GetObjectTagging",
+      "s3:GetObjectVersion",
       "s3:PutObjectTagging",
       "s3:PutObjectVersionTagging",
     ]
