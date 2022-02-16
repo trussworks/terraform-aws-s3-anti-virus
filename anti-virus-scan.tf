25196778f71e32feef9fc58b2367deac9fcfb8e9
Liran Ben Abu <liranb@diagnosticrobotics.com>
2022-02-16 11:47:53 +0200

    .

--- anti-virus-scan.tf
+++ anti-virus-scan.tf
@@ -41,12 +41,12 @@ data "aws_iam_policy_document" "main_scan" {
     effect = "Allow"
 
     actions = [
+      var.av_delete_infected_files ? "s3:DeleteObject,s3:GetObject" : "s3:GetObject",
       "s3:GetObject",
       "s3:GetObjectTagging",
       "s3:GetObjectVersion",
       "s3:PutObjectTagging",
-      "s3:PutObjectVersionTagging",
-      var.av_delete_infected_files ? "s3:DeleteObject" : "s3:GetObject"
+      "s3:PutObjectVersionTagging"
     ]
 
     resources = formatlist("%s/*", data.aws_s3_bucket.main_scan.*.arn)
